6a97f1c4393afc9442c67a58f3c8f802f6c4bdf4
abietic <iamyhc@live.cn>
2017-03-21 22:48:58 +0800

    Add files via upload

--- f0815.cpp
+++ f0815.cpp
@@ -2,17 +2,43 @@
 #include <fstream>
 using namespace std;
 //-------------------------------------------------------------
+class Matrix;
 class Vector
 {
 	private:
 		int* v;
 		int sz;
 	public:
-		void remove(){ delete[] v; }
+		Vector(Vector& t)
+		{
+			if(t.size()<=0)
+			{
+				cerr<<"invalid template\n";
+				exit(1);
+			}
+			sz = t.size();
+			v = new int[sz];
+			for(int i = 0;i<sz;i++)
+			{
+				v[i] = t[i];
+			}
+		}
+		Vector()
+		{
+			sz = 0;
+			v = NULL;
+		}
+		Vector(int s)
+		{
+			sz = s;
+			v = new int[sz];
+		}
+		~Vector(){ delete[] v; }
 		int size(){ return sz; }
 		void set(int);
 		void display();
 		int& operator[](int);
+		friend Vector multiply(Matrix& m,Vector& v);
 };
 //------------------------------------------------------------
 //-------------------------------------------------------------
@@ -54,11 +80,47 @@ class Matrix
 		int* m;
 		int szl,szr;
 	public:
+		Matrix(int x,int y)
+		{
+			if(x <=0|| y <= 0)
+			{
+				cerr<<"bad matrix size\n";
+				exit(1);
+			}
+			szl = x;
+			szr = y;
+			m = new int[x*y];
+		}
+		Matrix()
+		{
+			m = NULL;
+			szl = 0;
+			szr = 0;
+		}
+		Matrix(Matrix& t)
+		{
+			if(t.sizeL() <= 0||t.sizeR() <=0)
+			{
+				cerr<<"invalid template\n";
+				exit(1);
+			}
+			szl = t.sizeL();
+			szr = t.sizeR();
+			m = new int[szl*szr];
+			for(int i = 0;i<szl;i++)
+			{
+				for(int j = 0;j<szr;j++)
+				{
+					m[i*szl+j] = t.elem(i,j);
+				}
+			}
+		}
 		void set(int, int);
-		void remove(){ delete[] m; }
+		~Matrix(){ delete[] m; }
 		int sizeL(){ return szl; }
 		int sizeR(){ return szr; }
 		int& elem(int ,int);
+		friend Vector multiply(Matrix& m,Vector& v);
 };
 //--------------------------------------------------------------
 //--------------------------------------------------------------
@@ -86,19 +148,19 @@ int& Matrix::elem(int i, int j)
 //--------------------------------------------------------------
 Vector multiply(Matrix& m, Vector& v)
 {
-	if( m.sizeR() != v.size() )
+	if( m.szr != v.sz )
 	{
 		cerr<<"Bad multiply Matrix with Vector.\n";
 		exit(1);
 	}
 	Vector r;
-	r.set(m.sizeL());
-	for(int i = 0;i<m.sizeL();i++)
+	r.set(m.szl);
+	for(int i = 0;i<m.szl;i++)
 	{
 		r[i] = 0;
-		for( int j = 0;j<m.sizeR();j++)
+		for( int j = 0;j<m.szr;j++)
 		{
-			r[i] += m.elem(i,j) * v[j];
+			r[i] += m.m[i*(m.szr)+j] * v[j];
 		}
 	}
 	return r;
@@ -110,8 +172,7 @@ int main()
 	ifstream in("in");
 	int x,y;
 	in>>x>>y;
-	Matrix ma;
-	ma.set(x,y);
+	Matrix ma(x,y);
 	for(int i = 0;i<x;i++)
 	{
 		for(int j = 0;j<y;j++)
@@ -120,16 +181,11 @@ int main()
 		}
 	}
 	in>>x;
-	Vector ve;
-	ve.set(x);
+	Vector ve(x);
 	for(int i = 0;i<x;++i)
 	{
 		in>>ve[i];
 	}
-	Vector vx = multiply(ma,ve);
-	vx.display();
-	ma.remove();
-	ve.remove();
-	vx.remove();
+	multiply(ma,ve).display();
 	return 0;
 }
